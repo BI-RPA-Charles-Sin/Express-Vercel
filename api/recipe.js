a20894f78a8e52b8cc337d9292a13709f7adb3e3
Charles_Sin <charles_sin@mail.chungyo.net>
2023-05-08 16:48:22 +0800

    1. add mapping url 2. add read body key 3. add checking vaild token

--- api/recipe.js
+++ api/recipe.js
@@ -38,7 +38,14 @@ router.get("/traceredirect", (req, res, next) => {
 });
 
 router.post("/api/query_redirect_url", (req, res, next) => {
-  res.status(200).json({ status: "0000", result: "https://www.baidu.com/" });
+  const { decode_key } = req.body;
+  const vaild = { 5271675: "https://www.baidu.com/", 1189908: "http://sogou.com/", 6546491: "https://tieba.baidu.com/", 1487350: "https://www.sohu.com/", 1883964: "https://www.kafan.cn/" };
+
+  if (vaild.includes(decode_key)) {
+    res.status(200).json({ status: "0000", result: "https://www.baidu.com/" });
+  } else {
+    res.status(404).json({ status: "404", result: "Not Found" });
+  }
 });
 
 module.exports = router;
