a0a7faa98a94b20e953da229bdcd9fd3de4c72f2
Charles_Sin <charles_sin@mail.chungyo.net>
2023-05-08 17:13:10 +0800

    1. print out decode key

--- api/recipe.js
+++ api/recipe.js
@@ -40,9 +40,9 @@ router.get("/traceredirect", (req, res, next) => {
 router.post("/api/query_redirect_url", (req, res, next) => {
   const { decode_key } = req.body;
   const vaild = { 5271675: "https://www.baidu.com/", 1189908: "http://sogou.com/", 6546491: "https://tieba.baidu.com/", 1487350: "https://www.sohu.com/", 1883964: "https://www.kafan.cn/" };
+  console.log(vaild[decode_key]);
 
   if (vaild[decode_key]) {
-    console.log(vaild[decode_key]);
     res.status(200).json({ status: "0000", result: vaild[decode_key] });
   } else {
     res.status(204).json({ status: "404", result: "Not Found" });
